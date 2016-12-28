229535154cd1dae187d3ff216973d9b7d95aa4bc
Randall Krauskopf <randall.krauskopf@gmail.com>
2016-12-28 10:01:27 -0600

    -code clean up and comments -add stub for the spawnSync process

--- test/specs/syncSpec.js
+++ test/specs/syncSpec.js
@@ -1,78 +1,69 @@
 const expect = require('chai').expect;
 const proxyquire = require('proxyquire');
 const sinon = require('sinon');
-let child_process = require('child_process');
 
-let child_processMock = null;
+let childProcessMock = null;
 let sync = null;
-let output = null;
 
-describe.only('The sync function', () => {
+describe('The sync function', () => {
 
   beforeEach(() => {
 
-    sinon.spy(child_process, 'spawnSync');
+    childProcessMock = {
+      spawnSync: sinon.spy()
+    }
 
     sync = proxyquire('../../lib/sync.js', {
-      'child_process': child_process
+      'child_process': childProcessMock
     });
 
-  });
-
-  afterEach(() => {
-    child_process.spawnSync.restore();
   });
 
   it('should throw an error if no cli command is specified', () => {
     expect(sync.bind(null)).to.throw('Must specify command line program');
   });
 
-  //Ex. sync('my-app', '--foo', '--outFile=/foo bar/baz')
+  describe('when only the cli command is specified', () => {
+    it('should only call spawnSync with the cli command', () => {
+      sync('my-cli');
+      sinon.assert.calledOnce(childProcessMock.spawnSync);
+      sinon.assert.calledWithExactly(childProcessMock.spawnSync, 'my-cli', []);
+    });
+  });
+
+  //Ex. sync('my-cli', '--foo', '--outFile=/foo bar/baz')
   describe('when the cli is called with seperate strings', () => {
     beforeEach(() => {
-      output = sync('ls', '-al');
+      sync('my-cli', '--foo', '--outFile=/foo bar/baz');
     });
 
     it('should parse and execute the cli properly', () => {
-      sinon.assert.calledOnce(child_process.spawnSync);
-      sinon.assert.calledWith(child_process.spawnSync, 'ls', ['-al']);
+      sinon.assert.calledOnce(childProcessMock.spawnSync);
+      sinon.assert.calledWithExactly(childProcessMock.spawnSync, 'my-cli', ['--foo', '--outFile=/foo bar/baz']);
     });
 
-    it('should return an output object', () => {
-      expect(output).to.be.an('object');
-
-    });
   });
 
-  //Ex. sync('my-app', ['--foo', '--outFile', '/foo bar/baz'])
+  //Ex. sync('my-cli', ['--foo', '--outFile', '/foo bar/baz'])
   describe('When the cli is called with an array of arguments', () => {
     it('should parse and execute the cli properly', () => {
-      output = sync('ls', ['-al']);
+      sync('my-cli', ['--foo', '--outFile', '/foo bar/baz']);
 
-      sinon.assert.calledOnce(child_process.spawnSync);
-      sinon.assert.calledWith(child_process.spawnSync, 'ls', ['-al']);
+      sinon.assert.calledOnce(childProcessMock.spawnSync);
+      sinon.assert.calledWithExactly(childProcessMock.spawnSync, 'my-cli', ['--foo', '--outFile', '/foo bar/baz']);
     });
-
-    it('should return an output object', () => {
-      expect(output).to.be.an('object');
-    });
-
   });
 
-  //Ex. sync('my-app --foo --outFile="/foo bar/baz"')
+  //Ex. sync('my-cli --foo --outFile="/foo bar/baz"')
   describe('When the cli is called as one string', () => {
     beforeEach(() => {
-      output = sync('ls -al');
+      sync('my-cli --foo --outFile="/foo bar/baz"');
     });
 
     it('should parse and execute the cli properly', () => {
-      sinon.assert.calledOnce(child_process.spawnSync);
-      sinon.assert.calledWith(child_process.spawnSync, 'ls', ['-al']);
-    });
-
-    it('should return an output object', () => {
-      expect(output).to.be.an('object');
+      sinon.assert.calledOnce(childProcessMock.spawnSync);
+      sinon.assert.calledWithExactly(childProcessMock.spawnSync, 'my-cli', ['--foo', '--outFile="/foo bar/baz"']);
     });
   });
 
-})
+});
