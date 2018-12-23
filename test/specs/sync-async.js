d2dac6c837fc6fc01d01000f6911eff1b8218f9d
James Messinger <james.messinger@gmail.com>
2018-12-23 08:54:51 -0600

    Added tests for `chaiExecSync` and `chaiExecAsync`

---
+++ test/specs/sync-async.js
@@ -0,0 +1,58 @@
+"use strict";
+
+const { chaiExecSync, chaiExecAsync } = require("../../");
+const chai = require("chai");
+const expect = chai.expect;
+const assert = chai.assert;
+
+chai.use(chaiExecSync);
+chai.use(chaiExecAsync);
+chai.should();
+
+describe("chaiExecSync", () => {
+  it("should return synchronously", () => {
+    let cli = chaiExecSync("test/fixtures/bin/echo-args --foo --bar");
+
+    // Expect syntax
+    expect(cli).exit.code.to.equal(0);
+    expect(cli).stdout.to.equal("Argument #1: --foo\nArgument #2: --bar\n");
+    expect(cli).stderr.to.be.empty;
+    expect(cli).output.to.equal("Argument #1: --foo\nArgument #2: --bar\n");
+
+    // Should syntax
+    cli.should.have.exit.code(0);
+    cli.should.have.stdout.that.equals("Argument #1: --foo\nArgument #2: --bar\n");
+    cli.should.have.stderr.that.is.empty;
+    cli.should.have.output.that.equals("Argument #1: --foo\nArgument #2: --bar\n");
+
+    // Assert syntax
+    assert.exitCode(cli, 0);
+    assert.stdout(cli, "Argument #1: --foo\nArgument #2: --bar\n");
+    assert.stderr(cli, "");
+    assert.output(cli, "Argument #1: --foo\nArgument #2: --bar\n");
+  });
+});
+
+describe("chaiExecAsync", () => {
+  it("should return asynchronously", async () => {
+    let cli = await chaiExecAsync("test/fixtures/bin/echo-args --foo --bar");
+
+    // Expect syntax
+    expect(cli).exit.code.to.equal(0);
+    expect(cli).stdout.to.equal("Argument #1: --foo\nArgument #2: --bar\n");
+    expect(cli).stderr.to.be.empty;
+    expect(cli).output.to.equal("Argument #1: --foo\nArgument #2: --bar\n");
+
+    // Should syntax
+    cli.should.have.exit.code(0);
+    cli.should.have.stdout.that.equals("Argument #1: --foo\nArgument #2: --bar\n");
+    cli.should.have.stderr.that.is.empty;
+    cli.should.have.output.that.equals("Argument #1: --foo\nArgument #2: --bar\n");
+
+    // Assert syntax
+    assert.exitCode(cli, 0);
+    assert.stdout(cli, "Argument #1: --foo\nArgument #2: --bar\n");
+    assert.stderr(cli, "");
+    assert.output(cli, "Argument #1: --foo\nArgument #2: --bar\n");
+  });
+});
